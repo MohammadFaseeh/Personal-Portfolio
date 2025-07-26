660292a718d01254fe41449108b3752dc819dbcc
Mohammad Faseeh <145072594+MohammadFaseeh@users.noreply.github.com>
2025-07-26 17:10:22 +0500

    index.js

--- index.js
+++ index.js
@@ -1,44 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Faseeh - Portfolio</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/ad993e1f31.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <!-- Header -->
+    <div id="header">
+      <div class="container">
+        <nav>
+          <img src="images/logo.png" class="logo" />
+          <ul id="sidemenu">
+            <li><a href="#header">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <i class="fas fa-times" onclick="closemenu()"></i>
+          </ul>
+          <i class="fas fa-bars" onclick="openmenu()"></i>
+        </nav>
+        <div class="header-text">
+          <p>Hi, I'm</p>
+          <h1>Mohammad <span>Faseeh</span></h1>
+          <p>And I'm Software Developer</p>
+        </div>
+      </div>
+    </div>
 
-var tablinks = document.getElementsByClassName("tab-links");
-      var tabcontents = document.getElementsByClassName("tab-contents");
+    <!-- About --->
+    <div id="about">
+      <div class="container">
+        <div class="row">
+          <div class="about-col-1">
+            <img src="images/user.png" alt="" />
+          </div>
+          <div class="about-col-2">
+            <h1 class="sub-title">About Me</h1>
+            <p>
+              I’m Computer Science graduate with a strong passion for Software
+              development, AI enthusiastic, and cloud technologies. I specialize
+              in building modern, responsive web applications with AI
+              integration and continuously strive to learn and grow in the
+              ever-evolving tech world.
+            </p>
 
-      function opentab(tabname) {
-        for (tablink of tablinks) {
-          tablink.classList.remove("active-link");
-        }
-        for (tabcontent of tabcontents) {
-          tabcontent.classList.remove("active-tab");
-        }
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
-      }
+            <div class="tab-titles">
+              <p class="tab-links active-link" onclick="opentab('skills')">
+                Skills
+              </p>
+              <p class="tab-links" onclick="opentab('experience')">
+                Experience
+              </p>
+              <p class="tab-links" onclick="opentab('education')">Education</p>
+            </div>
+            <div class="tab-contents active-tab" id="skills">
+              <ul>
+                <li>
+                  <span>Web Development</span><br />Responsive full-stack web
+                  applications using MERN stack
+                </li>
+                <li>
+                  <span>AI & Python</span><br />AI-powered app development using
+                  Python, Flask, and Django
+                </li>
+                <li>
+                  <span>Cloud & DevOps</span><br />Working with Docker,
+                  Kubernetes, and cloud-based microservices
+                </li>
+              </ul>
+            </div>
+            <div class="tab-contents" id="experience">
+              <ul>
+                <li>
+                  <span>Internship - IT Department</span><br />
+                  Pakistan Ordnance Factories (POF), Wah Cantt<br />
+                  Contributed to Full Stack Web Development using MERN stack,
+                  assisted with database backups, and supported cloud-based
+                  solutions during internship.
+                </li>
+                <li>
+                  <span>Freelancing</span><br />
+                  Worked as a freelance developer on various web projects,
+                  integrating AI features and deploying applications using
+                  modern cloud technologies.
+                </li>
+              </ul>
+            </div>
+            <div class="tab-contents" id="education">
+              <ul>
+                <li>
+                  <span>Education</span><br />
+                  Bachelor of Science in Computer Science<br />
+                  Comsats University Islamabad, Abbottabad Campus<br />
+                  Graduated: 2025
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-      // side-Menu
-      var sidemeu = document.getElementById("sidemenu");
+    <!-- Services -->
+    <div id="services">
+      <div class="container">
+        <h1 class="sub-title">My Services</h1>
+        <div class="services-list">
+          <div>
+            <i class="fas fa-code"></i>
+            <h2>Full Stack Web Development</h2>
+            <p>
+              I build responsive and scalable web applications using the MERN
+              stack (MongoDB, Express.js, React.js, Node.js). My services cover
+              both frontend and backend development, API integration, Cloud
+              deployment, and performance optimization.
+            </p>
+            <a href="#">Learn more</a>
+          </div>
+          <div>
+            <i class="fas fa-brain"></i>
+            <h2>AI-Integration & Smart Features</h2>
+            <p>
+              I integrate AI tools like OpenAI GPT for intelligent features-such
+              as chatbots, Q&A with PDFs, and smart automation. Ideal for
+              startups and businesses wanting to enhance user interaction using
+              modern AI solutions.
+            </p>
+            <a href="#">Learn more</a>
+          </div>
+          <div>
+            <i class="fas fa-tools"></i>
+            <h2>Freelance Solutions </h2>
+            <p>
+              I provide professional freelance services-ranging from custom
+              website creation to bug fixing and database management. My
+              experience includes handling client projects and assisting IT
+              departments during internships with real-world issues.
+            </p>
+            <a href="#">Learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
-      function openmenu(){
-        sidemeu.style.right = "0";
+    <!-- Portfolio -->
+     <div id="portfolio">
+        <div class="container">
+            <h1 class="sub-title">My Work</h1>
+            <div class="work-list">
+                <div class="work">
+                    <img src="images/work-1.png" alt="">
+                    <div class="layer">
+                      <h3>Gemini Clone</h3>
+<p>An interactive AI chatbot clone inspired by Google Gemini.</p>
 
-      }
-      function closemenu(){
-        sidemeu.style.right = "-200px";
-        
-      }
+                      <a href="https://gemini-clone-ui.netlify.app"><i class="fas fa-external-link-alt"></i></a>
+                    </div>
+                </div>
+                <div class="work">
+                    <img src="images/work-2.png" alt="">
+                    <div class="layer">
+                      <h3>Staybnb</h3>
+<p>A modern vacation rental platform inspired by Airbnb.</p>
 
-      // msg
-      const scriptURL = 'https://script.google.com/macros/s/AKfycbxQfFyd3zin59YxEVpQ1ItJisO0rSQjwTrh0Yky7PZX8BDAn2XmAHK8qwwBeRWVw592/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
+                      <a href="https://staybnb-eight.vercel.app"><i class="fas fa-external-link-alt"></i></a>
+                    </div>
+                </div>
+                <!-- <div class="work">
+                    <img src="images/work-3.png" alt="">
+                    <div class="layer">
+                      <h3>Wedding Booking System</h3>
+                      <p>Manage wedding events and vendors with real-time booking system.</p>
+                      <a href="#"><i class="fas fa-external-link-alt"></i></a>
+                    </div>
+                </div> -->
+            </div>
+            <a href="#" class="btn">See more</a>
+        </div>
+     </div>
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML = "Message Sent Successfully"
-        setTimeout(function(){
-          msg.innerHTML = ""
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+     <!-- Contact -->
+      <div id="contact">
+        <div class="container">
+          <div class="row">
+            <div class="contact-left">
+<h1 class="subtitle">Contact Me</h1>
+<p><i class="fas fa-paper-plane"></i>rathorefaseeh7861@gmail.com</p>
+<p><i class="fas fa-phone-square-alt"></i>0314-5483717</p>
+<div class="social-icons">
+  <a href="https://www.linkedin.com/in/faseehrathore"><i class="fab fa-linkedin"></i></a>
+  <a href="https://github.com/MohammadFaseeh"><i class="fab fa-github"></i></a>
+  <a href="https://www.facebook.com/faseeh.rathore"><i class="fab fa-facebook"></i></a>
+  <a href="https://www.instagram.com/faseeh_ul_hasaan"><i class="fab fa-instagram"></i></a>
+</div>
+<a href="images/Cv.pdf" download class="btn btn2">Download CV</a>
+            </div>
+            <div class="contact-right">
+              <form name="submit-to-google-sheet">
+                <input type="text" name="Name" placeholder="Your Name" required>
+                <input type="email" name="Email" placeholder="Your Email" required>
+                <textarea name="Message" rows="6" placeholder="Your Message" required></textarea>
+                <button type="submit" class="btn btn2">Submit</button>
+              </form>
+              <span id="msg"></span>
+            </div>
+          </div>
+        </div>
+        <div class="copyright">
+            <p>Copyright © FASEEH.</p>
+          </div>
+      </div>
+
+    <script src="index.js"></script>
+  </body>
+</html>
